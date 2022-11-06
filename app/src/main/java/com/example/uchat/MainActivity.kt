396ea4e9a4a33581868fa86c92a8cdc72ca53834
Tarazi <200103456@stu.sdu.edu.kz>
2022-11-06 23:24:45 +0600

    add the authentication

--- app/src/main/java/com/example/uchat/MainActivity.kt
+++ app/src/main/java/com/example/uchat/MainActivity.kt
@@ -3,8 +3,7 @@ package com.example.uchat
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.uchat.databinding.ActivityMainBinding
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,8 +16,23 @@ class MainActivity : AppCompatActivity() {
         val myRef: DatabaseReference = database.getReference("message")
         myRef.setValue("Hello, World!")
 
-        bSend.setOnClickListener{
+        bSend.setOnClickListener {
             myRef.setValue(edMessage.text.toString())
         }
-    }
+        onChangeListener(myRef)
+
+        }
+        private fun onChangeListener(dRef: DatabaseReference){
+            dRef.addValueEventListener(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    rcView.append("\n")
+                    rcView.append("Sergey: ${snapshot.value.toString()}")
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+            })
+        }
 }
---
+++ app/src/main/java/com/example/uchat/SignInAct.kt
@@ -0,0 +1,44 @@
+package com.example.uchat
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.account.WorkAccount.getClient
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+
+class SignInAct : AppCompatActivity() {
+    lateinit var launcher: ActivityResultLauncher<Intent>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult){
+
+        }
+    }
+
+    private fun getClient(): GoogleSignInClient{
+        val gso = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(getString(R.string.your_web_client_id))
+                    // Only show accounts previously used to sign in.
+                    .setFilterByAuthorizedAccounts(true)
+                    .build())
+            .build()
+        return GoogleSignInClient.getClient(this, gso)
+    }
+
+    private fun signInWithGoogle(){
+        val signInClient = getClient()
+        launcher.launch(signInClient.signInIntent)
+    }
+
+    private fun
+}
