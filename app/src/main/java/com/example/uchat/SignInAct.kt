4fe0bca48048e9d6e11e19617aaa668e5acbc13f
Tarazi <200103456@stu.sdu.edu.kz>
2022-11-06 23:41:47 +0600

    add the authentication2

--- app/src/main/java/com/example/uchat/SignInAct.kt
+++ app/src/main/java/com/example/uchat/SignInAct.kt
@@ -3,19 +3,26 @@ package com.example.uchat
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.gms.auth.account.WorkAccount.getClient
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignInAct : AppCompatActivity() {
     lateinit var launcher: ActivityResultLauncher<Intent>
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        auth = Firebase.auth
         launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult){
 
         }
@@ -40,5 +47,14 @@ class SignInAct : AppCompatActivity() {
         launcher.launch(signInClient.signInIntent)
     }
 
-    private fun
+    private fun firebaseAuthWithGoogle(idToken: String){
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener(){
+            if(it.isSuccessful){
+                Log.d("MyLog","Google signIn done")
+            }else{
+                Log.d("MyLog","Google signIn error")
+            }
+        }
+    }
 }
